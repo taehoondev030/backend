670ffa646e5be67fe38ed769aa3fd66cb87d6d1c
taehoondev030 <taehoondev030@gmail.com>
2024-10-25 22:25:48 +0900

    Update 0007_auto_20241024_1248.py

--- app/core/migrations/0007_auto_20241024_1248.py
+++ app/core/migrations/0007_auto_20241024_1248.py
@@ -11,11 +11,11 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='user',
-            name='age',
-            field=models.PositiveIntegerField(blank=True, null=True),
-        ),
+        # migrations.AddField(
+        #     model_name='user',
+        #     name='age',
+        #     field=models.PositiveIntegerField(blank=True, null=True),
+        # ),
         migrations.AddField(
             model_name='user',
             name='gender',
